7468c826b21997101898f7db7037414f91f9a5dc
andreireporter13 <andrei.reporter13@gmail.com>
2022-07-03 17:00:58 +0300

    important

--- app.py
+++ app.py
@@ -113,5 +113,7 @@ def index():
     return render_template('index.html')
 
     
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    import warnings
+    warnings.warn("use 'python -m nltk', not 'python -m nltk.downloader'", DeprecationWarning)
+    app.run_server(debug=True)
